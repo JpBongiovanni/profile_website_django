2c2191f256c323d24cdb1f563340d5f4d8c0267f
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-04-13 21:21:39 +0900

    progress

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {
-        "wdd_img": media/world_diety_database_logo.jpg
-    }
-    return render(request, "index.html", context)
+    return render(request, "index.html")
 
 def about_me(request):
     return render(request, "about_me.html")
